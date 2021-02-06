088e89ff54650c1ab37919dca7caf7fc2c4c6c56
Jorge Arias <jariasbr@everis.com>
2021-02-06 13:11:47 +0100

    project setup completed

---
+++ MarvelMVP/AppAssembly.swift
@@ -0,0 +1,22 @@
+//
+//  AppAssembly.swift
+//  Marvel-MVP
+//
+//  Created by Jorge Arias Brasa on 06/02/2021.
+//
+
+import UIKit
+
+import UIKit
+
+internal final class AppAssembly {
+
+    // MARK: - Variables
+    private(set) lazy var window = UIWindow(frame: UIScreen.main.bounds)
+    private(set) lazy var coreAssembly = CoreAssembly(navigationController: navigationController)
+    private(set) lazy var navigationController: UINavigationController = {
+        return MNavigationController()
+    }()
+}
+
+
