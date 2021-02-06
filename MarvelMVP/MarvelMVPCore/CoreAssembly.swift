373d11506032eab2cb67eb760ad0a0dca2b5d429
Jorge Arias <jariasbr@everis.com>
2021-02-06 18:50:53 +0100

    ADD: Assembly Layer

--- MarvelMVP/MarvelMVPCore/CoreAssembly.swift
+++ MarvelMVP/MarvelMVPCore/CoreAssembly.swift
@@ -5,4 +5,24 @@
 //  Created by Jorge Arias Brasa on 06/02/2021.
 //
 
-import Foundation
+import UIKit
+
+final public class CoreAssembly {
+
+    // MARK: - Properties
+    private let navigationController: UINavigationController
+
+    // MARK: - Variables
+    public private(set) lazy var comicListAssembly =
+        ComicListAssembly(comicDetailAssembly: comicDetailAssembly,
+                                webServiceAssembly: webServiceAssembly)
+    
+    private(set) lazy var webServiceAssembly = WebServiceAssembly()
+
+    // MARK: - Initializers
+    public init(navigationController: UINavigationController) {
+
+        self.navigationController = navigationController
+    }
+}
+
---
+++ MarvelMVP/MarvelMVPCore/Features/ComicList/ComicListAssembly.swift
@@ -0,0 +1,35 @@
+//
+//  ComicListAssembly.swift
+//  Marvel-MVP
+//
+//  Created by Jorge Arias Brasa on 06/02/2021.
+//
+
+import UIKit
+
+public final class ComicListAssembly {
+
+    // MARK: - Properties
+    private let webServiceAssembly: WebServiceAssembly
+
+    // MARK: - Initializers
+    init(webServiceAssembly: WebServiceAssembly) {
+        self.webServiceAssembly = webServiceAssembly
+    }
+
+    // MARK: - Public Methods
+    public func viewController() -> UIViewController {
+        return ComicListViewController(presenter: presenter())
+    }
+
+    // MARK: - Internal Methods
+    func presenter() -> ComicListPresenterProtocol {
+        return ComicListPresenter(comicDetailNavigator: comicDetailAssembly.detailNavigator(),
+                                        comicListRepository: comicListRepository())
+    }
+
+    func comicListRepository() -> ComicListRepositoryProtocol {
+        return ComicListRepository(webService: webServiceAssembly.webService)
+    }
+}
+
